9116f3002ad8920b66689030c82046792ad3889c
whois3301 <145847294+whoisaqoke@users.noreply.github.com>
2025-11-19 21:50:59 +0300

    removed validation and cosmetic changes

--- lecture_2/main.py
+++ lecture_2/main.py
@@ -1,45 +1,36 @@
 
 def generate_profile(age):
     if 0<=age<=12:
-        a="Child"
-        return a
+        
+        return "Child"
     elif 13<=age<=19:
-        a="Teenager"
-        return a
+        
+        return "Teenager"
     elif age>=20:
-        a="Adult"
-        return a
-    else:
-        err = "your age must be >=0"
+        
+        return "Adult"
+    
+
+user_name = input("Enter your name: ")
+    
+
 while True:
-    user_name = input("Enter your name: ")
-    if user_name.strip() and not user_name.isdigit():
+    
+    birth_year_str = int(input("Enter your birth year: "))
+    if birth_year_str <= 2025:
         break
     else:
-        print("Name must be non-empty and not numeric\n")
-
-
-while True:
-    print ("enter your birth year")
-    
-    try:
-        birth_year_str = int(input())
-        if 1900 <= birth_year_str <= 2025:
-            break
-        else:
-            print("Year must be between 1900 and 2025\n")
+        print("Year must be between 1900 and 2025\n")
         break
         
-    except ValueError:
-        print ("year must be int\n")    
-
+    
 current_age = 2025 - birth_year_str
 
 hobbies=[]
 
 while True:
-    print("Enter your favorite hobby or type 'stop' to finish")
-    user_input = input()
+    
+    user_input = input("Enter your favorite hobby or type 'stop' to finish: ")
     if user_input == "stop":
         break
     else:
@@ -48,7 +39,7 @@ while True:
 
 life_stage=generate_profile(current_age)
 joined_hobbies = f"""
--""".join(hobbies)
+- """.join(hobbies)
 user_profile={
     'name':user_name,
     'age':current_age,
@@ -61,11 +52,11 @@ print (f"""
 Profile Summary:
 Name: {user_profile.get('name')}
 Age: {user_profile.get('age')}
-Life Stage:{user_profile.get('stage')}""")
+Life Stage: {user_profile.get('stage')}""")
 
 if len(hobbies)>0:
-    print(f"""Favotite Hobbies({len(hobbies)})
--{user_profile.get('hobbies')}
+    print(f"""Favotite Hobbies ({len(hobbies)}):
+- {user_profile.get('hobbies')}
 ---""")
 else:
     print(f"""Favotite Hobbies({len(hobbies)})
